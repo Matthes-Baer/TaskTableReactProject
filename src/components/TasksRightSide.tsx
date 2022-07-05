1d607e160cbc739bd5fe9fdb981fdc30a62d9d35
Matthes-Baer <Matthes.baer@yahoo.de>
2022-07-05 15:37:31 +0200

    added module.css file

--- src/components/TasksRightSide.tsx
+++ src/components/TasksRightSide.tsx
@@ -1,16 +1,18 @@
 import CompletedTaskContainer from '../components/CompletedTaskContainer';
 
 
+
 const TasksRightSide = () => {
     return (
         <>
             <div className="row p-4 d-flex justify-content-center align-items-center" style={{height: '375px'}}>
-                Testing
+                Test
+   
             </div>
             <div>
                 <CompletedTaskContainer />
             </div>
-            
+           
         </>
     )
 }
