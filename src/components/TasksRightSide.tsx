3408244f782c8f44424176e751704a1778cbbd9d
Matthes-Baer <Matthes.baer@yahoo.de>
2022-07-06 19:32:47 +0200

    test with new branch

--- src/components/TasksRightSide.tsx
+++ src/components/TasksRightSide.tsx
@@ -6,7 +6,7 @@ const TasksRightSide = () => {
     return (
         <>
             <div className="row p-4 d-flex justify-content-center align-items-center" style={{height: '375px'}}>
-                Test
+                Test2
    
             </div>
             <div>
