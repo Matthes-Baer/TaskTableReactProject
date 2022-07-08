898220c2da73992f402fac23319d338a66c324e9
Matthes-Baer <Matthes.baer@yahoo.de>
2022-07-08 16:53:14 +0200

    added timer for testing

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SideBar = (): JSX.Element => {
+    const [time, setTime] = useState<number>(600);
+    const [playing, setPlaying] = useState<boolean>(false);
+    let minutes = Math.floor(time / 60);
+    let seconds = (time - minutes * 60);
+    let formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+      useEffect(() => {
+        const timeout = setTimeout(() => {
+            if(time && playing){
+                setTime(time - 1)
+            }
+          }, 1000);
+
+          if (!playing) {
+            clearTimeout(timeout)
+          }
+      }, [playing, time])
     return (
         <>
             Testing with component
+            {formattedMinutes + ":" + formattedSeconds}
+            {playing.toString()}
+            <button onClick={() => setPlaying(!playing)}>Play/Stop</button>
         </>
     )
 }
