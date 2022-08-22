3ee2a4855dbd27c2c15eebe972040e6d0821b818
Matthes-Baer <Matthes.baer@yahoo.de>
2022-08-22 21:58:14 +0200

    delete: deleted detailTaskView stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-
-// imports regarding State
-import { addActiveTodo } from './features/ActiveTodosSlice';
-import { RootState } from './app/store';
 
 // Components and Routes import:
 import HomeRoute from './Routes/home';
 import ErrorPage from './Routes/ErrorPage';
 import Navbar from './components/Navbar';
-import DetailView from './Routes/DetailTaskView';
-
-// import { useGet5DayForecastQuery } from './APIs/WeatherApi';
 
 function App() {
-  const activeTodos = useSelector((state: RootState) => state.activeTodos.value)
-  const dispatch = useDispatch();
-
-  // const { data, isFetching } = useGet5DayForecastQuery({});
-
-  // console.log(data && data)
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navbar />}>
           <Route index element={<HomeRoute /> } />
-          <Route path=':taskID' element={<DetailView />} />
           <Route path='*' element={<ErrorPage />} />
         </Route>
       </Routes>
