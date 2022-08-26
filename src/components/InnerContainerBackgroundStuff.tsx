dde0c2eec03c9eca1c4a0b2a4c3a027290bcac52
Matthes-Baer <Matthes.baer@yahoo.de>
2022-08-26 21:40:52 +0200

    feat: Added first moving rectangle in background

--- src/components/InnerContainerBackgroundStuff.tsx
+++ src/components/InnerContainerBackgroundStuff.tsx
@@ -1,8 +1,22 @@
+import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react";
 import { useSelector } from "react-redux"
 import { RootState } from "../app/store"
 
 export default function InnerContainerBackgroundStuff() {
     const darkmode = useSelector((state: RootState) => state.colorTheme.value);
+    const movingRectangleRef1 = useRef(null);
+    const movingRectangleRef2 = useRef(null);
+    
+    useLayoutEffect(() => {
+        var movingRectangleRef1Timeline = gsap.timeline({ repeat: Infinity });
+        movingRectangleRef1Timeline.to(movingRectangleRef1.current, { x: 0, y: 0, rotate: 0, perspective: 1000, duration: 1 });
+        movingRectangleRef1Timeline.to(movingRectangleRef1.current, { x: 30, y: 35, rotate: 25, perspective: 1000, duration: 10 });
+        movingRectangleRef1Timeline.to(movingRectangleRef1.current, { x: 70, y: 35, rotate: 35, perspective: 1000, duration: 12 });
+        movingRectangleRef1Timeline.to(movingRectangleRef1.current, { x: 25, y: 15, rotate: 75, perspective: 1000, rotateZ: 20, duration: 15 });
+        movingRectangleRef1Timeline.to(movingRectangleRef1.current, { x: 35, y: 25, rotate: 45, perspective: 1000, rotateZ: 15, duration: 13 });
+        movingRectangleRef1Timeline.to(movingRectangleRef1.current, { x: 0, y: 0, rotate: 0, perspective: 1000, duration: 7.5 });
+    }, []);
 
     return (    
         <div className="position-absolute" style={{top: 0, left: 0}}>
@@ -82,6 +96,36 @@ export default function InnerContainerBackgroundStuff() {
                 right: 0,
                 zIndex: 0,
                 }}>
+            </div>
+
+            <div style={{
+                width: '100px',
+                height: '100px',
+                backgroundColor: 'transparent',
+                border: '3px solid #EC214E',
+                position: 'absolute',
+                top: 200,
+                left: 50,
+                opacity: 0.5,
+                zIndex: 0,
+                overflow: 'hidden'
+            }}
+            ref={movingRectangleRef1}>
+            </div>
+
+            <div style={{
+                width: '100px',
+                height: '100px',
+                backgroundColor: 'transparent',
+                border: '3px solid #EC214E',
+                position: 'absolute',
+                top: 200,
+                left: 50,
+                opacity: 0.5,
+                zIndex: 0,
+                overflow: 'hidden'
+            }}
+            ref={movingRectangleRef2}>
             </div>
         </div>
     )
