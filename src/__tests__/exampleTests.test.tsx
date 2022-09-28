5438e039c959e9f93b62fd5f66442bd9f5be0c86
Matthes-Baer <Matthes.baer@yahoo.de>
2022-09-28 11:45:11 +0200

    feat: Added own render function for Testing with Redux

--- src/__tests__/exampleTests.test.tsx
+++ src/__tests__/exampleTests.test.tsx
@@ -1,7 +1,12 @@
-import Sum from "../testFunctions/testFunction";
+import { fireEvent, screen } from "@testing-library/react";
+// We're using our own custom render function and not RTL's render.
+import { renderWithProviders } from "../utils/test-utils";
+import App from "../App";
 
-describe("sum", () => {
-  test("sums up to two values", () => {
-    expect(Sum(1, 2)).toBe(3);
-  });
+// We use msw to intercept the network request during the test,
+// and return the response 'John Smith' after 150ms
+// when receiving a get request to the `/api/user` endpoint
+
+test("Render App Component", async () => {
+  renderWithProviders(<App />);
 });
--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,6 +1,6 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
-import { ActiveTodosSlice } from '../features/ActiveTodosSlice';
+import { ActiveTodosSlice } from "../features/ActiveTodosSlice";
 import { ColorSlice } from "../features/ColorSlice";
 import { DoneTodoSlice } from "../features/DoneTodoSlice";
 import { CurrentTimeSlice } from "../features/CurrentTimeSlice";
@@ -9,20 +9,19 @@ import { CurrentTimeSlice } from "../features/CurrentTimeSlice";
 // import { QuoteAPI } from "../APIs/QuoteAPI";
 // import { WeatherApi } from '../APIs/WeatherApi';
 
-
 export const store = configureStore({
-    reducer: {
-        activeTodos: ActiveTodosSlice.reducer,
-        colorTheme: ColorSlice.reducer,
-        doneTodos: DoneTodoSlice.reducer,
-        currentTime: CurrentTimeSlice.reducer,
-        // quoteAPI: QuoteAPI.reducer,
-        // WeatherApi: WeatherApi.reducer,
-    },
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(QuoteAPI.middleware)
-})
+  reducer: {
+    activeTodos: ActiveTodosSlice.reducer,
+    colorTheme: ColorSlice.reducer,
+    doneTodos: DoneTodoSlice.reducer,
+    currentTime: CurrentTimeSlice.reducer,
+    // quoteAPI: QuoteAPI.reducer,
+    // WeatherApi: WeatherApi.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(QuoteAPI.middleware)
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
