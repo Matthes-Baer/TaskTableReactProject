ef5d628ee01418ad6c1f46618d26098bfed76ded
Matthes-Baer <Matthes.baer@yahoo.de>
2022-09-29 19:41:52 +0200

    feat: Test for CompletedTaskContainer Component

--- src/__tests__/exampleTests.test.tsx
+++ src/__tests__/exampleTests.test.tsx
@@ -1,12 +1,15 @@
-import { fireEvent, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 // We're using our own custom render function and not RTL's render.
 import { renderWithProviders } from "../utils/test-utils";
 import App from "../App";
-
-// We use msw to intercept the network request during the test,
-// and return the response 'John Smith' after 150ms
-// when receiving a get request to the `/api/user` endpoint
+import CompletedTaskContainer from "../components/CompletedTaskContainer";
 
 test("Render App Component", async () => {
   renderWithProviders(<App />);
+});
+
+test("render CompletedTaskContainer", async () => {
+  renderWithProviders(<CompletedTaskContainer />);
+  const Element = screen.getByText(/Finished Tasks/i);
+  expect(Element).toBeInTheDocument();
 });
