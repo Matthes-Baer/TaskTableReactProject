743a2fb2f9d8a4022b40856e59b857edd4eb8c69
Matthes-Baer <Matthes.baer@yahoo.de>
2023-06-24 02:19:09 +0200

    feat: minor change

--- src/features/ActiveTodosSlice.tsx
+++ src/features/ActiveTodosSlice.tsx
@@ -1,33 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface activeTodoArrayInterface {
-    value: activeTodoInterface[];
+  value: activeTodoInterface[];
 }
 
 interface activeTodoInterface {
-    id: string
-    title: string
-    badges?: {name: string, checked: boolean}[]
-    comment?: string
-    time: number
+  id: string;
+  title: string;
+  badges?: { name: string; checked: boolean }[];
+  comment?: string;
+  time: number;
 }
 
 const initialState: activeTodoArrayInterface = {
-    value: []
+  value: [],
 };
 
 export const ActiveTodosSlice = createSlice({
-    name: 'activeTodos',
-    initialState,
-    reducers: {
-        addActiveTodo: (state, action: PayloadAction<activeTodoInterface>) => {
-            state.value.push(action.payload);
-        },
-        removeActiveTodo: (state, action: PayloadAction<number>) => {
-            state.value.splice(action.payload, 1);
-        }
-    }
-})
+  name: "activeTodos",
+  initialState,
+  reducers: {
+    addActiveTodo: (state, action: PayloadAction<activeTodoInterface>) => {
+      state.value.push(action.payload);
+    },
+    removeActiveTodo: (state, action: PayloadAction<number>) => {
+      state.value.splice(action.payload, 1);
+    },
+  },
+});
 
 export const { addActiveTodo, removeActiveTodo } = ActiveTodosSlice.actions;
 
